2193b6b2963e506b7d3acc46ddd8b89ec995c2ec
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-29 17:09:30 +0000

    automatically backed up by learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -32,7 +32,7 @@ class PetsController < ApplicationController
   post '/pets/:id' do
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
-    if !params["owner"].empty?
+    if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(:name => params["owner"][:name])
     end
     @pet.save
